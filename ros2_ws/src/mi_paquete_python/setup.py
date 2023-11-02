45a17b7e04ad2ad2463ce9c5335e736c4afd2dd4
SimonBC3 <sbrotons3@gmail.com>
2023-11-02 11:21:43 +0100

    circle_action_client started

--- ros2_ws/src/mi_paquete_python/setup.py
+++ ros2_ws/src/mi_paquete_python/setup.py
@@ -29,7 +29,8 @@ setup(
             'turtle_controller = mi_paquete_python.turtle_controller:main',
             'move_turtle = mi_paquete_python.move_turtle:main',
             'move_turtle_client = mi_paquete_python.move_turtle_client:main',
-            'circle_action_server = mi_paquete_python.circle_action_server:main'
+            'circle_action_server = mi_paquete_python.circle_action_server:main',
+            'circle_action_client = mi_paquete_python.circle_action_client:main'
         ],
     },
 )
