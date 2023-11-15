85fcdeb49319249595ca718390505c40760e779a
SimonBC3 <sbrotons3@gmail.com>
2023-11-15 12:45:20 +0100

    part 1 finished patrolling and going to exit

--- ros2_ws/src/project/project/service_server.py
+++ ros2_ws/src/project/project/service_server.py
@@ -4,6 +4,10 @@ import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist, Point
 from turtlesim.msg import Pose
+from nav2_simple_commander.robot_navigator import BasicNavigator
+from geometry_msgs.msg import PoseStamped
+
+import tf_transformations
 
 
 class ServiceServer(Node):
@@ -12,24 +16,67 @@ class ServiceServer(Node):
         self.srv = self.create_service(
             MoveRobot, "robot_service", self.service_callback
         )
-        self.subscriber = self.create_subscription(
-            Pose, "/turtle1/pose", self.listener_callback, 10
-        )
+
+        self.nav = BasicNavigator()
+        initial_pose = self.create_pose_stamped(self.nav, -1.576505, -0.792324, 0.008534)
+        self.nav.setInitialPose(initial_pose)
+        self.nav.waitUntilNav2Active()
 
     def service_callback(self, request, response):
         self.get_logger().info(f"Incoming request: {request}")
-        response.answer = 'nain'
+        response.answer = 'No eligible command'
+
+        if request.command == 'patrol':
+            response.answer = self.patrol()
+        elif request.command == 'goToExit':
+            response.answer = self.go_to_exit()
 
         return response
 
-    def listener_callback(self, response):
-        self.get_logger().info(f"Listening to Pose topic: {response}")
-
     def patrol(self):
-        return
+        entrance_corridor = self.create_pose_stamped(self.nav, 1.1, 0.35, 0.0)
+        bottom_living_room = self.create_pose_stamped(self.nav, -1.4, 3.5, 0.0) 
+        bottom_left_room = self.create_pose_stamped(self.nav, -6.3, 3.3, 0.0)
+        bottom_right_room = self.create_pose_stamped(self.nav, -6.3, -1.65, 0.0)
+        top_room = self.create_pose_stamped(self.nav, 6.279, -0.99, 0.0)
+        top_living_room = self.create_pose_stamped(self.nav, 3.39, 0.88, 0.0)
+        mid_room = self.create_pose_stamped(self.nav, 1.04, 3.57, 0.0)
+        self.goal_final_position = self.create_pose_stamped(self.nav, -1.36, -3.08, 0.0)
+        
+        waypoints = [entrance_corridor, bottom_living_room, bottom_left_room, bottom_right_room,
+                     bottom_left_room, bottom_living_room, entrance_corridor, top_living_room, 
+                     top_room, top_living_room, mid_room, top_living_room, entrance_corridor, self.goal_final_position]
+
+        self.nav.followWaypoints(waypoints)
+        while not self.nav.isTaskComplete():
+            feedback = self.nav.getFeedback()
+            self.get_logger().info(f"Feedback of the robot: {feedback}")
+
+        self.get_logger().info(f"Feedback of the robot: {self.nav.getResult()}")
+        
+        return str(self.nav.getResult())
     
     def go_to_exit(self):
-        return
+        self.nav.goToPose(self.goal_final_position)
+        while not self.nav.isTaskComplete():
+            feedback = self.nav.getFeedback()
+            self.get_logger().info(f"Feedback of the robot: {feedback}")
+
+        return str(self.nav.getResult())
+
+    def create_pose_stamped(self, navigator, position_x, position_y, rotation_z):
+        q_x, q_y, q_z, q_w = tf_transformations.quaternion_from_euler(0.0, 0.0, rotation_z)
+        goal_pose = PoseStamped()
+        goal_pose.header.frame_id = "map"
+        goal_pose.header.stamp = navigator.get_clock().now().to_msg()
+        goal_pose.pose.position.x = position_x
+        goal_pose.pose.position.y = position_y
+        goal_pose.pose.position.z = 0.0
+        goal_pose.pose.orientation.x = q_x
+        goal_pose.pose.orientation.y = q_y
+        goal_pose.pose.orientation.z = q_z
+        goal_pose.pose.orientation.w = q_w
+        return goal_pose
 
 
 def main(args=None):
--- ros2_ws/src/project/project/test_client.py
+++ ros2_ws/src/project/project/test_client.py
@@ -8,14 +8,23 @@ class TestClient(Node):
     def __init__(self):
         super().__init__('robot_client')
         self.client = self.create_client(MoveRobot, 'robot_service')
-        
         self.request = MoveRobot.Request()
+
+        selection = ''
+        while selection != '0' or selection != '1':
+            selection = str(input("\n\r0 to patrol \n\r 1 to go to exit\n\r"))
+            if selection == '0':
+                self.request.command = 'patrol'
+            elif selection == '1':
+                self.request.command = 'goToExit'
+            else:
+                print('Wrong selection...')
+
         print(self.request)
 
         while not self.client.wait_for_service(timeout_sec=1.0):
             self.get_logger().info('service not available, waiting again...')
         
-
     def send_request(self):
         future = self.client.call_async(self.request)
         future.add_done_callback(self.done_callback)
@@ -23,7 +32,7 @@ class TestClient(Node):
     def done_callback(self, future):
         try: 
             response = future.result()
-            self.get_logger().info(f"Result {response}")
+            self.get_logger().info(f"Result: {response}")
             rclpy.shutdown()
         except Exception as e:
             self.get_logger().error(f"Service call failed { e} ")
